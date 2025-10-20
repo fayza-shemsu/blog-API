5536ed7bcb022ba948c2ea6e4b42ee256279ba6b
fayza shemsu <fafibintshem@gmail.com>
2025-10-20 14:47:34 +0300

    Configured server and updated dependencies

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+
+require ("dotenv").config();
 const express = require ("express");
+const connectDB =require ("./config/db");
+const authRoutes = require ("./routes/authRoutes");
 const app =express();
 
 app.use(express.json());
 
+connectDB();
+
 app.get("/",(req,res) =>{
     res.send ( "Welcome  to Blog API");
 });
+ app.use("/api/auth",authRoutes);
 
 app.listen(5000, () =>{
     console.log("Server is running on port 5000");
